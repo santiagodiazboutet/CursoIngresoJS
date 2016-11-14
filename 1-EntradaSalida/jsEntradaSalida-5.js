0e5110829fed54b8e45f9f9e3a42e6225983357a
santiagodiazboutet <santiagodiazboutet@gmail.com>
2016-11-14 19:18:37 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-5.js
+++ 1-EntradaSalida/jsEntradaSalida-5.js
@@ -1,7 +1,10 @@
 /*Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
-function Mostar()
+function Mostrar()
 {	
-	
+	var dat=document.getElementById('elNombre').value;
+
+	var dat2=document.getElementById('laEdad').value;
+	alert("Usted se llama "+dat+" y tiene "+dat2+" años")
 }
 
