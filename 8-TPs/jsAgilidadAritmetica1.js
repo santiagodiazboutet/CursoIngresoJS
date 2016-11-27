ef22ac364ed7d574c938e170f87f8212ea85065e
santiagodiazboutet <santiagodiazboutet@gmail.com>
2016-11-27 13:43:55 -0300

    completados

--- 8-TPs/jsAgilidadAritmetica1.js
+++ 8-TPs/jsAgilidadAritmetica1.js
@@ -7,14 +7,50 @@ y una de las cuatro operaciones básicas (suma, resta,
 se debe informar si el resultado es el correcto o no.
 */
 var respuesta;
+var numeroUno;
+var Operador;
+var numeroDos;
 function comenzar()
 {
+numeroUno=Math.floor((Math.random()*10)+1);
+numeroDos=Math.floor((Math.random()*10)+1);
+Operador=Math.floor((Math.random()*4)+1);
+numeroUno=parseInt(numeroUno);
+numeroDos=parseInt(numeroDos);
+document.getElementById('PrimerNumero').value=numeroUno;
+document.getElementById('SegundoNumero').value=numeroDos;
+if (Operador==1)
+{
+	Operador="+";
+	respuesta=numeroUno+numeroDos;
+}else if (Operador == 2)
+{
+	Operador="*";
+	respuesta=numeroDos*numeroUno
+}else if (Operador==3)
+{
+	Operador="/";
+	respuesta=numeroUno/numeroDos;
+}else
+{
+	Operador="-";
+	respuesta=numeroUno-numeroDos
+}
+document.getElementById('Operador').value=Operador;
 
-	
-
+console.log(respuesta);
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
-	
+	var respuestaUsr;
+	respuestaUsr=document.getElementById('Respuesta').value;
+	if (respuestaUsr==respuesta)
+	{
+		alert("El resultado es correcto.");
+	}else
+	{
+		alert("El resultado es incorrecto.")
+	}
+
 
 }//FIN DE LA FUNCIÓN
--- 8-TPs/jsAgilidadAritmetica2.js
+++ 8-TPs/jsAgilidadAritmetica2.js
@@ -4,14 +4,51 @@ se agregara un temporizador que
  el juego  de no ser ingresado el resultado 
  correcto en ese lapso de tiempo. */
 var respuesta;
-var temporizador;
+var numeroUno;
+var Operador;
+var numeroDos;
 function comenzar()
 {
-	
+numeroUno=Math.floor((Math.random()*10)+1);
+numeroDos=Math.floor((Math.random()*10)+1);
+Operador=Math.floor((Math.random()*4)+1);
+numeroUno=parseInt(numeroUno);
+numeroDos=parseInt(numeroDos);
+document.getElementById('PrimerNumero').value=numeroUno;
+document.getElementById('SegundoNumero').value=numeroDos;
+	if (Operador==1)
+	{
+		Operador="+";
+		respuesta=numeroUno+numeroDos;
+	}else if (Operador == 2)
+	{
+		Operador="*";
+		respuesta=numeroDos*numeroUno
+	}else if (Operador==3)
+	{
+		Operador="/";
+		respuesta=numeroUno/numeroDos;
+	}else
+	{
+		Operador="-";
+		respuesta=numeroUno-numeroDos
+	}
+	document.getElementById('Operador').value=Operador;
+	window.setTimeout(Responder,4000)
+
+console.log(respuesta);
 }//FIN DE LA FUNCIÓN
 function Responder()
 {
-	
+	var respuestaUsr;
+	respuestaUsr=document.getElementById('Respuesta').value;
+	if (respuestaUsr==respuesta)
+	{
+		alert("El resultado es correcto.");
+	}else
+	{
+		alert("El resultado es incorrecto.")
+	}
 
 
 }//FIN DE LA FUNCIÓN
